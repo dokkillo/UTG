ff2a2daec1da4878bacac176b3f38b048d7d6007
Gabriel C <devgcosta@gmail.com>
2016-06-01 08:54:41 +0200

    Changes

---
+++ UTG/Universe/Generate/SystemSolarGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTG.Planet.Objects;
+using UTG.Utils;
+
+namespace UTG.Universe.Generate
+{
+    public static class SystemSolarGenerator
+    {
+        public static List<PlanetData> Init()
+        {
+            List<PlanetData> PlanetList = new List<PlanetData>();
+            var numPlanets = MathUtil.GetRandomNumber(1, 10);
+
+
+
+
+            return PlanetList;
+        }
+    }
+}
