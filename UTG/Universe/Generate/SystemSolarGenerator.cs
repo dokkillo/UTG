5379e4b7d0f7d7afb32d243dd1a362245ff18aab
Gabriel C <devgcosta@gmail.com>
2016-06-08 00:27:56 +0200

    fix

--- UTG/Universe/Generate/SystemSolarGenerator.cs
+++ UTG/Universe/Generate/SystemSolarGenerator.cs
@@ -12,6 +12,8 @@ namespace UTG.Universe.Generate
     public static class SystemSolarGenerator
     {
         private const int MAX_NUM_PLANETS = 10;
+
+
         public static List<PlanetData> Init()
         {
             List<PlanetData> PlanetList = new List<PlanetData>();
